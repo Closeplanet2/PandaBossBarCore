366f880c94af64c3aafea48e4ef7d8eea5d51836
closeplanet2 <68482902+closeplanet2@users.noreply.github.com>
2023-09-20 23:29:37 +0100

    Test Maven

--- src/main/java/com/closeplanet2/pandabossbarcore/PandaBossBarCore.java
+++ src/main/java/com/closeplanet2/pandabossbarcore/PandaBossBarCore.java
@@ -10,7 +10,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.HashMap;
-
+//
 public class PandaBossBarCore extends JavaPlugin {
     public static HashMap<String, PandaBossBar> BossBar_Register = new HashMap<>();
     public static BossBarSettings bossBarSettings;
